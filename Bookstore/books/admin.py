c4ca7969cb034aa6a8d6e8c666c56b2781c65f5f
niharika.shetty26 <niharika.shetty@successive.tech>
2024-08-09 14:24:52 +0530

    9th_August

--- Bookstore/books/admin.py
+++ Bookstore/books/admin.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 from .models import Book, UserProfile
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'price', 'genre')  # Fields to display in the admin list view
-    search_fields = ('title', 'author', 'genre')  # Fields to search by in the admin
+    list_display = ('title', 'author', 'price', 'genre')
+    search_fields = ('title', 'author', 'genre')
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role')  # Display user and their role in the admin panel
-    search_fields = ('user__username', 'role')  # Search functionality
+    list_display = ('user', 'role')
+    search_fields = ('user__username', 'role')
 
-# Register the Book model with the admin site
 admin.site.register(Book, BookAdmin)
-# Register the UserProfile model with the admin site
 admin.site.register(UserProfile, UserProfileAdmin)
